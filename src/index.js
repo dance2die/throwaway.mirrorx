2743db38b7ec7e6633fb12392713f9aa5731c011
Sung Kim <sungmkim80@gmail.com>
2018-07-12 21:28:16 -0400

    Got MirrorX working with @MinimumViablePerson 's code on https://stackblitz.com/edit/sample-app-mirror?file=models%2FappModel.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import "./styles.css";
 const Post = styled.div`
   display: flex;
   flex-direction: column;
+  border: 1px dotted #ccc;
+  padding: 1rem;
+  margin-bottom: 1rem;
+
+  &:hover {
+    background-color: #c0ff33;
+  }
 `;
 
 mirror.model({
@@ -15,36 +22,42 @@ mirror.model({
     posts: []
   },
   reducers: {
-    addPost: (state, post) => {
-      console.log(`addPost state=${state.posts}, post=${post}`);
-      return { ...state.posts, post };
-    }
+    addPosts: (state, posts) => ({ ...state, posts })
   },
   effects: {
-    async addPosts() {
-      actions.app.addPost(1);
-      actions.app.addPost(2);
-      actions.app.addPost(3);
+    async getAllPosts() {
+      const posts = await fetch(
+        "https://jsonplaceholder.typicode.com/posts/"
+      ).then(response => response.json());
+      actions.app.addPosts(posts);
     }
   }
 });
 
 class App extends Component {
   componentDidMount = async () => {
-    await actions.app.addPosts();
+    await actions.app.getAllPosts();
   };
 
   render() {
     console.log(this.props);
     return (
       // <div>{this.props.posts.map(post => <Post key={post} post={post} />)}</div>
-      <div>Hi!</div>
+      <div>
+        <h1>post IDs!</h1>
+        {this.props.posts.map(post => (
+          <Post key={post.id}>
+            <h3>{post.title}</h3>
+            <div>{post.body}</div>
+          </Post>
+        ))}
+      </div>
     );
   }
 }
 
 const AppWrapper = connect(state => {
-  return { posts: state.app };
+  return { posts: state.app.posts };
 })(App);
 
 render(<AppWrapper />, document.getElementById("root"));
